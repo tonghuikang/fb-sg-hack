d6ead64cb4a771c9969abdd66630cb51615441cd
Tong Hui Kang <tonghuikang@outlook.com>
2018-09-02 21:52:53 +0800

    Now sequential

--- debug-js/script.js
+++ debug-js/script.js
@@ -75,69 +75,71 @@ function updateLabel() {
       console.log("Google Vision API - OCR results");
       console.log(jsonParsed);
       var result_ocr = jsonParsed.responses[0].fullTextAnnotation.text;
+      result_ocr = result_ocr.replace(/\n/g, " ");  // replace newlines with space
       console.log(result_ocr);
       searchString += result_ocr;
       // I don't know how to line break within paragraph cell :/
       label.textContent = label.textContent + " --------------------------------------------------------------- result_ocr : " + result_ocr;
       // console.log(jsonParsed.items[0].formattedUrl);
-    }
-  };
-  
-  // Using Google Natural Language API for entity sentiment anaysis
-  // (why 'entity sentiment' - it captures words like 'evil' unlike 'entity' only)
-  // TODO next: extract the verb as well, entity-sentiment after syntax for now
-  // Then combine them into the result that is to be passed into 
-  // -- TODO later: we want verb extraction (syntax) to be run concurrently
-  // Future work: search the Claim Review directly - given-text-detect-claim
-  
-  var xhr_keyword = new XMLHttpRequest();
-  var data_keyword = {
-    "encodingType": "UTF8",
-    "document": {
-      "type": "PLAIN_TEXT",
-      "content": searchString
-    }
-  };
-  var body_keyword = JSON.stringify(data_keyword);
-  xhr_keyword.open("POST", 'https://language.googleapis.com/v1/documents:analyzeEntitySentiment?key=AIzaSyD6Csdss9VNdvqtu2rOJ0n19nZC6pHk-_E', true);
-  xhr_keyword.setRequestHeader('Content-Type', 'application/json');
-  xhr_keyword.send(body_keyword);
-  xhr_keyword.onreadystatechange = function() {
-    if (xhr_keyword.readyState === 4)  {
-      var serverResponse = xhr_keyword.responseText;
-      var jsonParsed = JSON.parse(serverResponse);
-      // okay I can now get any formatted result that I want from Google Custom Search
-      // Now we need to extract the relevant search keywords, the next step is Google cloud language
-      console.log("Google Natural Language API - entity sentiment results");
-      console.log(jsonParsed);
-      var result_keyword = jsonParsed.entities[0].name;  // WILL ITERATE AND GET ALL THE NAMES
-      console.log(result_keyword);
-      // I don't know how to line break within paragraph cell :/
-      label.textContent = label.textContent + " --------------------------------------------------------------- result_keyword : " + result_keyword;
-    }
-  };
-  
-  // Using Google Custom Search API to search certain fact-checking sites
-  // The top three results will be shown beside the timestamp
-  
-  var xhr_search = new XMLHttpRequest();
-  xhr_search.open("GET", 'https://www.googleapis.com/customsearch/v1?key=AIzaSyD6Csdss9VNdvqtu2rOJ0n19nZC6pHk-_E&cx=006556501642864997360:yq85mpbgvaq&q=' + searchString, true);
-  xhr_search.send(null);
-  xhr_search.onreadystatechange = function() {
-    if (xhr_search.readyState === 4)  {
-      var serverResponse = xhr_search.responseText;
-      var jsonParsed = JSON.parse(serverResponse);
-      // okay I can now get any formatted result that I want from Google Custom Search
-      // Now we need to extract the relevant search keywords, the next step is Google cloud language
-      console.log("Google Custom Search results");
-      console.log(jsonParsed);
-      var result_search = jsonParsed.items[0].formattedUrl;  
-      console.log(result_search);
-      // I don't know how to line break within paragraph cell :/
-      label.textContent = label.textContent + " --------------------------------------------------------------- result_search : " + result_search;
+    
+    
+    // Using Google Natural Language API for entity sentiment anaysis
+    // (why 'entity sentiment' - it captures words like 'evil' unlike 'entity' only)
+    // TODO next: extract the verb as well, entity-sentiment after syntax for now
+    // Then combine them into the result that is to be passed into 
+    // -- TODO later: we want verb extraction (syntax) to be run concurrently
+    // Future work: search the Claim Review directly - given-text-detect-claim
+
+    var xhr_keyword = new XMLHttpRequest();
+    var data_keyword = {
+      "encodingType": "UTF8",
+      "document": {
+        "type": "PLAIN_TEXT",
+        "content": searchString
+      }
     };
-  };
-  // display result
+    var body_keyword = JSON.stringify(data_keyword);
+    xhr_keyword.open("POST", 'https://language.googleapis.com/v1/documents:analyzeEntitySentiment?key=AIzaSyD6Csdss9VNdvqtu2rOJ0n19nZC6pHk-_E', true);
+    xhr_keyword.setRequestHeader('Content-Type', 'application/json');
+    xhr_keyword.send(body_keyword);
+    xhr_keyword.onreadystatechange = function() {
+      if (xhr_keyword.readyState === 4)  {
+        var serverResponse = xhr_keyword.responseText;
+        var jsonParsed = JSON.parse(serverResponse);
+        // okay I can now get any formatted result that I want from Google Custom Search
+        // Now we need to extract the relevant search keywords, the next step is Google cloud language
+        console.log("Google Natural Language API - entity sentiment results");
+        console.log(jsonParsed);
+        var result_keyword = jsonParsed.entities[0].name;  // WILL ITERATE AND GET ALL THE NAMES
+        console.log(result_keyword);
+        // I don't know how to line break within paragraph cell :/
+        label.textContent = label.textContent + " --------------------------------------------------------------- result_keyword : " + result_keyword;
+      
+    
+    // Using Google Custom Search API to search certain fact-checking sites
+    // The top three results will be shown beside the timestamp
+
+    var xhr_search = new XMLHttpRequest();
+    xhr_search.open("GET", 'https://www.googleapis.com/customsearch/v1?key=AIzaSyD6Csdss9VNdvqtu2rOJ0n19nZC6pHk-_E&cx=006556501642864997360:yq85mpbgvaq&q=' + searchString, true);
+    xhr_search.send(null);
+    xhr_search.onreadystatechange = function() {
+      if (xhr_search.readyState === 4)  {
+        var serverResponse = xhr_search.responseText;
+        var jsonParsed = JSON.parse(serverResponse);
+        // okay I can now get any formatted result that I want from Google Custom Search
+        // Now we need to extract the relevant search keywords, the next step is Google cloud language
+        console.log("Google Custom Search results");
+        console.log(jsonParsed);
+        var result_search = jsonParsed.items[0].formattedUrl;  
+        console.log(result_search);
+        // I don't know how to line break within paragraph cell :/
+        label.textContent = label.textContent + " --------------------------------------------------------------- result_search : " + result_search;
+      };
+    }; // search
+      };}; // keywords
+    };}; // OCR
+  
+  
   label.textContent = displayedResult;
   
 }
