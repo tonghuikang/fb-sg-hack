1ff58e6f4cd268b1ade72bf542facfe28750383c
Jerry <ace1972083891@gmail.com>
2018-09-08 23:00:31 +0800

    fake_news_identifier

--- fake_news_identifier/content.js
+++ fake_news_identifier/content.js
@@ -19,6 +19,58 @@ var find_text = function(source, class_name){
 	return output
 }
 
+var add_checkbox = function(source){
+	var time_position = source.getElementsByClassName("_5pcp _5lel _2jyu _232_");
+	var checkbox_positive = document.createElement("input");
+	checkbox_positive.type = "checkbox";
+	checkbox_positive.name = "positive_box";
+	
+	var checkbox_negative = document.createElement("input");
+	checkbox_negative.type = "checkbox";
+	checkbox_negative.name = "negative_box";
+	
+	var label = document.createElement("label");
+	label.appendChild(document.createTextNode("Is the article relevant?"));
+	var label_yes = document.createElement("label");
+	label_yes.appendChild(document.createTextNode("YES"));
+	var label_no = document.createElement("label");
+	label_no.appendChild(document.createTextNode("No?"));
+	
+	time_position[0].appendChild(label);
+	time_position[0].appendChild(checkbox_positive);
+	time_position[0].appendChild(label_yes);
+	time_position[0].appendChild(checkbox_negative);
+	time_position[0].appendChild(label_no);
+	
+	checkbox_positive.addEventListener("change", function(){
+		if (this.checked){
+			console.log("positive");
+		}
+	})
+	checkbox_negative.addEventListener("change", function(){
+		if (this.checked){
+			console.log("negative")
+		}
+	})
+}
+
+
+
+var b64 = function (url, cb) {
+    var image = new Image();
+    image.setAttribute('crossOrigin', 'anonymous');
+    image.onload = function () {
+      var canvas = document.createElement('canvas');
+      canvas.height = this.naturalHeight;
+      canvas.width = this.naturalWidth;
+      canvas.getContext('2d').drawImage(this, 0, 0);
+      var b64data = canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, '');
+      cb(b64data);
+    };
+    image.src = url;
+  };
+
+
 
 var fact_checker = function(location, image_url, add_text) {
 
@@ -27,6 +79,7 @@ var fact_checker = function(location, image_url, add_text) {
     }
 
     var displayedResult = image_url + '\n' + add_text;
+    var data_ocr = {};
 
     /////////////
     //// OCR ////
@@ -35,32 +88,35 @@ var fact_checker = function(location, image_url, add_text) {
     // Using TEXT_DETECTION instead of DOCUMENT_TEXT_DETECTION, no particular reason
 
     var xhr_ocr = new XMLHttpRequest();
-    var data_ocr = {
-        "requests": [{
-            "features": [{
-                "type": "TEXT_DETECTION"
-            }],
-            "image": {
-                "source": {
-                    "imageUri": image_url
-                }
-            }
-        }]
-    };
+
+    b64(image_url, function (b64data) {
+        var data = {
+            "requests": [{
+              "image": {"content": b64data},
+              "features": [{'type': 'TEXT_DETECTION'}]
+            }]
+          };
+        data_ocr = data;
+
+    console.log(data_ocr);
+
     var body_ocr = JSON.stringify(data_ocr);
     xhr_ocr.open("POST", 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyD6Csdss9VNdvqtu2rOJ0n19nZC6pHk-_E', true);
     xhr_ocr.setRequestHeader('Content-Type', 'application/json');
     xhr_ocr.send(body_ocr);
     xhr_ocr.onreadystatechange = function() {
         if (xhr_ocr.readyState === 4) {
-            // var serverResponse = xhr_ocr.responseText;
-            // var jsonParsed = JSON.parse(serverResponse);
-            // // okay I can now get any formatted result that I want from Google Custom Search
-            // // Now we need to extract the relevant search keywords, the next step is Google cloud language
-            // console.log("Google Vision API - OCR results");
-            // console.log(jsonParsed);
-			// var result_ocr = jsonParsed.responses[0].fullTextAnnotation.text;
-			var result_ocr = ''; // NOTHING FOR NOW
+            var serverResponse = xhr_ocr.responseText;
+            var jsonParsed = JSON.parse(serverResponse);
+            // okay I can now get any formatted result that I want from Google Custom Search
+            // Now we need to extract the relevant search keywords, the next step is Google cloud language
+            console.log("Google Vision API - OCR results");
+            console.log(jsonParsed);
+            if (jsonParsed.responses[0].hasOwnProperty('fullTextAnnotation')){
+                var result_ocr = jsonParsed.responses[0].fullTextAnnotation.text;
+            } else {
+                var result_ocr = '';
+            }
             // BUG - SPOILS WHEN THE IMAGES DOES NOT CONTAIN ANY TEXT.
             result_ocr = result_ocr.replace(/\n/g, " "); // replace newlines with space
             console.log(result_ocr);
@@ -168,16 +224,16 @@ var fact_checker = function(location, image_url, add_text) {
                                     console.log(jsonParsed);
                                     var fact_checks_list = [];
                                     for (var index in jsonParsed.items){
-                                      var fact_check_title = jsonParsed.items[index].title;
-                                      var fact_check_link = jsonParsed.items[index].link;
-                                      console.log(fact_check_title);
-                                      console.log(fact_check_link);
-									  fact_checks_list.push(fact_check_link + ':' + fact_check_title + ' --- ');
-									  add_link(location, fact_check_link, fact_check_title);
+                                    var fact_check_title = jsonParsed.items[index].title;
+                                    var fact_check_link = jsonParsed.items[index].link;
+                                    console.log(fact_check_title);
+                                    console.log(fact_check_link);
+                                    fact_checks_list.push(fact_check_link + ':' + fact_check_title + ' --- ');
+                                    add_link(location, fact_check_link, fact_check_title);
                                     }
                                     var fact_checks_string = fact_checks_list.join(' ');
                                     // label.textContent = label.textContent + longDivider + " result_search : " + fact_checks_string;
-								};
+                                };
                             }; // search
                         };
                     }; // keywords-verbs
@@ -186,6 +242,7 @@ var fact_checker = function(location, image_url, add_text) {
         };
     }; // OCR
 
+    }); // b64 image loading
 
     // label.textContent = displayedResult;
 
@@ -247,6 +304,7 @@ var find_post = function(){
 				}
 
 				fact_checker(shared[0], photo, description_text);
+				add_checkbox(shared[0]);
 			}
 			
 			
@@ -263,6 +321,7 @@ var find_post = function(){
 					var photo = find_video(is_video);
 				}
 				fact_checker(post, photo, description_text);
+				add_checkbox(post);
 			}
 		}
 	}
